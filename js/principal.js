24b2f129bebda748f4191b773695732f51399485
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-08-07 12:07:53 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,14 @@
+console.log("Fui carregado de um arquivo externo!");
+var titulo = document.querySelector("#h2");
+titulo.textContent = "Meus clientes"; // mudando o conteudo do elemento do h2
+
+/**
+ * CALCULANDO O IMC
+ */
+
+
+
+/**
+ * ADICIONANDO UM EVENTO PARA QUANDO O USUARIO CLICAR NO BOTAO DE ADICIONAR CLIENTE
+ */
+
